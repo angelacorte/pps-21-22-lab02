84878cff615f337fda7f6112100ffdd84e1386a3
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-03-08 11:48:05 +0100

    made composeGeneric more generic

--- src/main/scala/u02lab/Es5.scala
+++ src/main/scala/u02lab/Es5.scala
@@ -5,9 +5,9 @@ object Es5 extends App:
   def compose(f:Int => Int, g:Int => Int): Int => Int=
     (x:Int) => f(g(x))
 
-  def composeGeneric[X](f: X => X, g:X => X): X => X =
+  def composeGeneric[X, Y, Z](f: Y => Z, g:X => Y): X => Z =
     (x:X) => f(g(x))
 
   println(compose(_ - 1, _ * 2)(5)) //9
-  println(composeGeneric[Int](_ - 1, _ * 2)(5))
-  println(composeGeneric[String](_ + "!", _ + "World")("Hello "))
+  println(composeGeneric((n:Int) => n + " toString", (n:Int) => n * 2)(7))
+
