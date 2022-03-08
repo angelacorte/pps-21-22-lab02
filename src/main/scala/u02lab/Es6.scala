bd51c3a6ee83680a3ee27c6946ec987a1c5c4c4f
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-03-08 11:52:21 +0100

    refactor code

--- src/main/scala/u02lab/Es6.scala
+++ src/main/scala/u02lab/Es6.scala
@@ -3,9 +3,9 @@ package u02lab
 object Es6 extends App:
 
   //@annotation.tailrec makes sure that this recursion is not a tail one
-  def fibonacci(x:Int): Int = x match
-    case x if x < 2 => x
-    case x if x > 1 =>  fibonacci(x-1) + fibonacci(x-2)
+  def fibonacci(x:Int): Int = x < 2 match
+    case true => x
+    case false =>  fibonacci(x-1) + fibonacci(x-2)
 
   println(fibonacci(0)) //0
   println(fibonacci(10)) //55
